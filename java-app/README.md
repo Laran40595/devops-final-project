a99d1bb1248d7c3f759bcbcbd76f17178ab28a41
Laran40595 <greatbillonaire@gmail.com>
2026-07-09 06:10:46 +0100

    Add Java app, Ansible playbook, and Docker Compose

---
+++ java-app/README.md
@@ -0,0 +1,15 @@
+# Java Portfolio Application
+
+This is a simple Java Maven application included as part of the DevOps Final Project.
+
+## Technologies
+
+- Java 17
+- Maven
+- Docker
+- Jenkins
+- Terraform
+- Ansible
+- AWS EC2
+
+This application demonstrates the Java component of the DevOps project and serves as a sample application that can be built and deployed through a CI/CD pipeline.
---
+++ java-app/src/main/java/com/portfolio/App.java
@@ -0,0 +1,19 @@
+package com.portfolio;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("DevOps Portfolio Application");
+
+        System.out.println("Built with:");
+        System.out.println("- Java");
+        System.out.println("- Maven");
+        System.out.println("- Docker");
+        System.out.println("- Jenkins");
+        System.out.println("- Terraform");
+        System.out.println("- Ansible");
+        System.out.println("- AWS EC2");
+
+    }
+}
